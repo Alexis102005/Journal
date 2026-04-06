c16c17db19d8f57fced93e0baf60479bcefe004d
Alexis Atindehou <alexisatindehou18@gmail.com>
2026-04-06 20:00:17 +0000

    fix

--- api/liturgie.js
+++ api/liturgie.js
@@ -1,13 +1,9 @@
-exports.handler = async () => {
+export default async function handler(req, res) {
   const date = new Date()
   const dateStr = `${date.getFullYear()}-${String(date.getMonth()+1).padStart(2,'0')}-${String(date.getDate()).padStart(2,'0')}`
   
   const response = await fetch(`https://api.aelf.org/v1/messes/${dateStr}/france`)
   const data = await response.json()
   
-  return {
-    statusCode: 200,
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(data)
-  }
+  res.status(200).json(data)
 }
